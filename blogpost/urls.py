031d0766ce46c545210da9e8a5b4f31c9334d123
kaede_momiji <44082622+kaedefuto@users.noreply.github.com>
2021-09-11 00:11:45 +0900

    Update urls.py

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
 """
 from django.urls import path
 
-from .views import BlogList, BlogDetail, BlogCreate, BlogDelete, BlogUpdate, signupview, loginview, logoutview, homeview, guest, PasswordChange, PasswordChangeDone, UserDelete, UserDeleteView
-#,BlogCreate2
+from .views import BlogList, BlogDetail, BlogCreate, BlogDelete, BlogUpdate, signupview, loginview, logoutview, homeview, guest, PasswordChange, PasswordChangeDone, UserDeleteView
+#,BlogCreate2UserDelete, 
 
 #from .views import ListView
 
@@ -19,7 +19,7 @@ from .views import BlogList, BlogDetail, BlogCreate, BlogDelete, BlogUpdate, sig
 urlpatterns = [
     path('list/', BlogList.as_view(), name='list'),
     #path('list/', ListView, name='list'),
-    path('/detail/<int:pk>/', BlogDetail.as_view(), name='detail'),
+    path('detail/<int:pk>/', BlogDetail.as_view(), name='detail'),
     path('create/', BlogCreate.as_view(), name='create'),
     path('delete/<int:pk>', BlogDelete.as_view(), name='delete'),
     #path('userdelete/<int:pk>', UserDelete.as_view(), name='userdelete'),
